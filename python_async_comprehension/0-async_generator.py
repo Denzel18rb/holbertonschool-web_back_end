355844eee7b1e84dce3ae236ed1aeb4e8c0b44ae
Denzel18rb <74129875@gmail.com>
2024-01-29 10:37:16 -0500

    add to 0-async_generator.py

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module defines an asynchronous generator function"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """This function generates random float numbers between 0 and 10 with"""
+    for _ in range(10):
+        """A 1-second delay between each generation"""
+        await asyncio.sleep(1)
+        yield random.random() * 10
